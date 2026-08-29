d428f020993e5d324ff333ac8b24cf0228648043
Michele <benedicmichele@gmail.com>
2026-08-29 17:49:30 +0200

    Translation of C src from Tic-Tac-Toe's game.c in go

--- internal/game/game.go
+++ internal/game/game.go
@@ -34,3 +34,85 @@ func print_board(board [BOARD_SIZE][BOARD_SIZE]rune) {
 	}
 	fmt.Println()
 }
+
+func read_index(label string, out *int) bool {
+	var input string
+	fmt.Print(label + ": ")
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		return false
+	}
+
+	var value int
+	_, err = fmt.Sscanf(input, "%d", &value)
+	if err != nil {
+		return false
+	}
+	if value < 0 || value >= BOARD_SIZE {
+		return false
+	}
+
+	*out = value
+	return true
+}
+
+func player_number(marker rune) int {
+	if marker == 'X' {
+		return 1
+	} else {
+		return 2
+	}
+}
+
+func player_move(marker rune, board [BOARD_SIZE][BOARD_SIZE]rune) {
+	print_board(board)
+	fmt.Printf("Player %d (%c), insert the coordinates of your move:\n",
+		player_number(marker), marker)
+
+	for {
+		var move action
+
+		if !read_index("row (0-2)", &move.row) ||
+			!read_index("column (0-2)", &move.col) {
+			fmt.Println("Coordinates not available, try again.")
+			continue
+		}
+		if board[move.row][move.col] != ' ' {
+			fmt.Println("Square already occupied, insert new coordinates.")
+			continue
+		}
+
+		board[move.row][move.col] = marker
+		return
+	}
+}
+
+func winner(board [BOARD_SIZE][BOARD_SIZE]rune) rune {
+	for i := 0; i < BOARD_SIZE; i++ {
+		if board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2] {
+			return board[i][0]
+		}
+		if board[0][i] != ' ' && board[0][i] == board[1][i] && board[1][i] == board[2][i] {
+			return board[0][i]
+		}
+	}
+	if board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2] {
+		return board[0][0]
+	}
+	if board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0] {
+		return board[0][2]
+	}
+
+	return ' '
+}
+
+func board_full(board [BOARD_SIZE][BOARD_SIZE]rune) bool {
+	for row := 0; row < BOARD_SIZE; row++ {
+		for col := 0; col < BOARD_SIZE; col++ {
+			if board[row][col] == ' ' {
+				return false
+			}
+		}
+	}
+	return true
+}
